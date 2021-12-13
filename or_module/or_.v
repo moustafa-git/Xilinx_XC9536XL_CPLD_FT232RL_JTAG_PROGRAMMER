14ece9ead7f9eb062465c1a5352b8a056c509c82
Mostafa <mostax266@gmail.com>
2021-12-13 23:43:39 +0200

    Implement OR module

---
+++ or_module/or_.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    23:39:30 12/13/2021 
+// Design Name: 
+// Module Name:    or_ 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module or_(input a,
+input b,
+output c
+    );
+or(c,a,b);
+
+endmodule
